935775c2b3588d69b98fb3f462387e75452c5dc7
ravibrock <66334356+ravibrock@users.noreply.github.com>
2023-03-12 16:16:24 -0500

    Change from conda to miniforge

--- zsh/functions/conda_initialization.zsh
+++ zsh/functions/conda_initialization.zsh
@@ -1,18 +1,17 @@
 if [ "$CONDA_INITIALIZED" = 0 ]
 then
     # Conda initialization
-    __conda_setup="$(~'/opt/anaconda3/bin/conda' 'shell.zsh' 'hook' 2> /dev/null)"
+    __conda_setup="$('/Users/ravibrock/miniforge3/bin/conda' 'shell.zsh' 'hook' 2> /dev/null)"
     if [ $? -eq 0 ]; then
         eval "$__conda_setup"
     else
-        if [ -f '~/opt/anaconda3/etc/profile.d/conda.sh' ]; then
-            . '~/opt/anaconda3/etc/profile.d/conda.sh'
+        if [ -f "/Users/ravibrock/miniforge3/etc/profile.d/conda.sh" ]; then
+            . "/Users/ravibrock/miniforge3/etc/profile.d/conda.sh"
         else
-            export PATH='~/opt/anaconda3/bin:$PATH'
+            export PATH="/Users/ravibrock/miniforge3/bin:$PATH"
         fi
     fi
     unset __conda_setup
-
     # Sets environment variable so that Conda setup is not run again
     export CONDA_INITIALIZED=1
 fi
