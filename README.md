12a60e8c434d9da222037c7cc338d8040c43b98d
ravibrock <66334356+ravibrock@users.noreply.github.com>
2023-04-20 20:31:24 -0500

    Add setup script

--- README.md
+++ README.md
@@ -1,26 +1,20 @@
 # Dotfiles
 Dotfiles that I've spent time configuring.
 
-## Git
-Create a `.gitignore_local` file in your home directory that contains Git info.
+## Setup
+After cloning the repo, `cd` into it, run `bash setup.sh`, and follow the prompts.
 
-## Homebrew
-Run `brew bundle --file=$DOTFILES/.brewfile` to install all packages and casks.
+### Git
+Add personal credentials to `git/.gitignore_local`.
 
-## iTerm2
+### iTerm2
 In `Settings` > `General` > `Preferences` enable "Load preferences from a custom folder or URL" and change to the iTerm2 folder.
 
-## Raycast
+### Raycast
 The `.rayconfig` file is encrypted since it contains credentials. I left it in the repo for personal use but it isn't accessible otherwise.
 
-## Vim
-I wrote `.vimrc` myself but borrowed from Amix's setup [here](https://github.com/amix/vimrc).
-
-## VSCode
-Symlink `.vscode.json` to `~/Library/Application Support/Code/User/settings.json`.
-
-## Zsh
-Create a `.zprivate` file in the `zsh` folder for loading things like API keys that should be kept offline.
+### Zsh
+Add API keys and personal info to `zsh/.zprivate`.
 
 ## *Disclaimer*
 *All of these are set up for MacOS and my personal computer. I can't guarantee that they'll work on another OS or machine.*
---
+++ setup.sh
@@ -0,0 +1,73 @@
+#!/usr/bin/env bash
+
+# Sets current path
+pushd "$(dirname "$(readlink -f "$BASH_SOURCE")")" > /dev/null && {
+    DIR="$PWD"
+    popd > /dev/null
+}
+
+# Defines function that symlinks within repo
+function link_in_repo() {
+    ln -sf $DIR/.brewfile $DIR/brewfile
+    ln -sf $DIR/.rayconfig $DIR/rayconfig
+    ln -sf $DIR/.vimrc $DIR/vimrc
+    ln -sf $DIR/.vscode.json $DIR/vscode.json
+    ln -sf $DIR/git/.gitconfig $DIR/git/gitconfig
+    ln -sf $DIR/git/.gitignore_global $DIR/git/gitignore_global
+    ln -sf $DIR/zsh/.zprofile $DIR/zsh/zprofile
+    ln -sf $DIR/zsh/.zshalias $DIR/zsh/zshalias
+    ln -sf $DIR/zsh/.zshenv $DIR/zsh/zshenv
+    ln -sf $DIR/zsh/.zshrc $DIR/zsh/zshrc
+}
+
+# Defines function that symlinks into home directory
+function link_to_home() {
+    ln -sf $DIR/.vimrc $HOME/.vimrc
+    ln -sf $DIR/git/.gitconfig $HOME/.gitconfig
+    ln -sf $DIR/git/.gitignore_global $HOME/.gitignore_global
+    ln -sf $DIR/.vscode.json $HOME/.config/Code/User/settings.json
+    ln -sf $DIR/zsh/.zprofile $HOME/.zprofile
+    ln -sf $DIR/zsh/.zshenv $HOME/.zshenv
+    ln -sf $DIR/zsh/.zshrc $HOME/.zshrc
+}
+
+# Install vim-plug
+function install_plug() {
+    printf "Install vim-plug? [y/n]: "
+    read -r response
+    if [[ "$response" == "y" ]]; then
+        curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+    fi
+}
+
+# Initialize private files
+function private_files() {
+    printf "[user]\n    name = [GIT USERNAME HERE]\n    email = [GIT EMAIL HERE]\n    signingkey = [GIT GPG KEYID HERE]" > $DIR/git/.gitconfig_local
+    printf "export PRIVATE_VARIABLE=\"[PRIVATE VARIABLE HERE]\"" > $DIR/zsh/.zprivate
+}
+
+# OS specific commands
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    printf "Install Homebrew and Brewfile packages? [y/n]: "
+    read -r response
+    if [[ "$response" == "y" ]]; then
+        /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+        brew bundle --file=$DIR/.brewfile
+    fi
+    link_in_repo
+    link_to_home
+    install_plug
+    private_files
+    ln -sf $DIR/.vscode.json $HOME/Library/Application\ Support/Code/User/settings.json
+elif [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    printf "This set of dotfiles was designed for MacOS. You'll need to manually install packages and symlink \`.vscode.json\`. Proceed with setup? [y/n]: "
+    read -r response
+    if [[ "$response" == "y" ]]; then
+        link_in_repo
+        link_to_home
+        install_plug
+        private_files
+    fi
+else
+    echo "Unsupported OS. Use Mac or Linux."
+fi
