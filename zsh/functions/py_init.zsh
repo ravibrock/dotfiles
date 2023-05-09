4b7cebfaf24a94aeb423329f2930377f83aa23fd
ravibrock <66334356+ravibrock@users.noreply.github.com>
2023-05-09 08:30:29 -0500

    Update py_init.zsh

--- zsh/functions/py_init.zsh
+++ zsh/functions/py_init.zsh
@@ -1,14 +1,14 @@
 if [ "$CONDA_INITIALIZED" = 0 ]
 then
     # Conda initialization
-    __conda_setup="$('/Users/ravibrock/miniforge3/bin/conda' 'shell.zsh' 'hook' 2> /dev/null)"
+    __conda_setup="$("$HOME/miniforge3/bin/conda" 'shell.zsh' 'hook' 2> /dev/null)"
     if [ $? -eq 0 ]; then
         eval "$__conda_setup"
     else
-        if [ -f "/Users/ravibrock/miniforge3/etc/profile.d/conda.sh" ]; then
-            . "/Users/ravibrock/miniforge3/etc/profile.d/conda.sh"
+        if [ -f "$HOME/miniforge3/etc/profile.d/conda.sh" ]; then
+            . "$HOME/miniforge3/etc/profile.d/conda.sh"
         else
-            export PATH="/Users/ravibrock/miniforge3/bin:$PATH"
+            export PATH="$HOME/miniforge3/bin:$PATH"
         fi
     fi
     unset __conda_setup
