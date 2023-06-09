6b02b4db96f233a2e1c21ac655d9998783d3795c
ravibrock <66334356+ravibrock@users.noreply.github.com>
2023-06-09 12:56:28 -0400

    Update base nvim config

--- nvim/init.lua
+++ nvim/init.lua
@@ -2,7 +2,6 @@
 require("config.keybindings")
 require("config.functionality")
 require("config.autocommands")
-require("config.functions")
 require("config.appearance")
 
 -- Plugins
--- nvim/lua/config/autocommands.lua
+++ nvim/lua/config/autocommands.lua
@@ -5,13 +5,46 @@ autocmd("BufEnter", {
     command = ":setlocal formatoptions-=ro"
 })
 
--- Disable spellcheck highlighting
-autocmd("BufEnter", {
-    command = ":highlight clear SpellBad SpellCap SpellRare SpellLocal"
-})
-
 -- Enable autoread
 vim.opt.autoread = true
 autocmd("FocusGained, BufEnter", {
     command = ":silent! checktime"
+})
+
+-- Opens symlinks in their target:
+function follow_symlink()
+    local file = vim.fn.expand("%:p")
+    local real_file = vim.fn.resolve(file)
+    if (string.match(real_file, "[^%d]+:/") or real_file == file) then
+        return
+    end
+    vim.cmd("file " .. real_file)
+    vim.cmd("echohl WarningMsg | echomsg 'Resolved symlink " .. file:gsub(os.getenv("HOME"), "~") .. " ——⟶ " .. real_file:gsub(os.getenv("HOME"), "~") .. "' | echohl None")
+    vim.cmd("silent! w!")  -- Workaround for "file exists" error on write
+end
+autocmd("BufRead", {
+    command = "lua follow_symlink()"
+})
+
+-- Disable highlighting
+function clear_spellcheck()
+    vim.cmd("highlight clear SpellBad")
+    vim.cmd("highlight clear SpellCap")
+    vim.cmd("highlight clear SpellRare")
+    vim.cmd("highlight clear SpellLocal")
+end
+autocmd("BufReadPre", {
+    command = ":lua clear_spellcheck()"
+})
+
+-- Remove trailing whitespace on save
+function clean_extra_spaces()
+    local save_cursor = vim.fn.getpos(".")
+    local old_query = vim.fn.getreg("/")
+    vim.cmd("silent %s/\\s\\+$//e")
+    vim.fn.setpos(".", save_cursor)
+    vim.fn.setreg("/", old_query)
+end
+autocmd("BufWritePre", {
+    command = "lua clean_extra_spaces()"
 })
