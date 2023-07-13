351d8e8126d5b82d05840f82b13adfa927935eff
ravibrock <66334356+ravibrock@users.noreply.github.com>
2023-07-13 16:44:25 -0400

    Update nvim dashboard

--- nvim/lua/plugins/opts/appearance/alpha-nvim.lua
+++ nvim/lua/plugins/opts/appearance/alpha-nvim.lua
@@ -3,12 +3,12 @@ all.opts = function()
     local dashboard = require("alpha.themes.dashboard")
     dashboard.section.header.val = require("ascii").art.text.neovim.dos_rebel
     dashboard.section.buttons.val = {
-        dashboard.button("f", " " .. " Find file", ":Telescope find_files <CR>"),
         dashboard.button("n", " " .. " New file", ":ene <BAR> startinsert <CR>"),
         dashboard.button("r", " " .. " Recent files", ":Telescope oldfiles <CR>"),
+        dashboard.button("f", "󰍉 " .. " Find file", ":Telescope find_files <CR>"),
         dashboard.button("g", " " .. " Find text", ":Telescope live_grep <CR>"),
-        dashboard.button("c", " " .. " Config", ":e $MYVIMRC <CR>"),
         dashboard.button("s", " " .. " Restore Session", [[:lua require("persistence").load() <cr>]]),
+        dashboard.button("c", " " .. " Config", ":e $MYVIMRC <CR>"),
         dashboard.button("m", "󱊍 " .. " Mason", ":Mason<CR>"),
         dashboard.button("l", "󰒲 " .. " Lazy", ":Lazy<CR>"),
         dashboard.button("q", " " .. " Quit", ":qa<CR>"),
