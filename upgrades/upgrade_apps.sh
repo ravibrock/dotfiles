7ca858d425819deb0e3e7f9e6ca14c2163eb210e
ravibrock <66334356+ravibrock@users.noreply.github.com>
2023-10-11 18:10:29 -0400

    Update .brewfile and upgrade_apps.sh

--- upgrades/upgrade_apps.sh
+++ upgrades/upgrade_apps.sh
@@ -2,23 +2,23 @@
 
 # Updates formulae and casks
 $BREW_PREFIX/bin/brew upgrade
-$BREW_PREFIX/bin/brew upgrade --cask
+$BREW_PREFIX/bin/brew cu --all --cleanup --no-brew-update --no-quarantine --yes
 
 # Updates App Store apps
 OUTDATED=$($BREW_PREFIX/bin/mas outdated | /usr/bin/awk '{print $2}')
 if [[ ! -z $OUTDATED ]]; then
     /usr/bin/killall $(echo $OUTDATED)
     $BREW_PREFIX/bin/mas upgrade
-    cd /Applications/
-    /usr/bin/open -a $(echo $OUTDATED)
 fi
 
 # Updates Homebrew itself
 $BREW_PREFIX/bin/brew update
 
+# Dumps brewfile
+$BREW_PREFIX/bin/brew bundle dump --force --file=$DOTFILES/.brewfile
+
 # Cleans up old versions
 $BREW_PREFIX/bin/brew cleanup --prune=all
-/usr/bin/find $BREW_PREFIX/Caskroom -type f '(' -name '*.dmg' -or -name '*.pkg' ')' -delete
 
 # Updates tldr documentation
 $BREW_PREFIX/bin/tldr --verbose --update
