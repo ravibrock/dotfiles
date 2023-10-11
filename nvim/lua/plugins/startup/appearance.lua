f96370db27dd099831bb7d7b8c6cd7dd79b8cab5
ravibrock <66334356+ravibrock@users.noreply.github.com>
2023-10-11 18:15:45 -0400

    Update indent-blankline.nvim

--- nvim/lua/plugins/startup/appearance.lua
+++ nvim/lua/plugins/startup/appearance.lua
@@ -140,18 +140,24 @@ return {
     {
         "lukas-reineke/indent-blankline.nvim",
         event = { "BufReadPost", "BufNewFile" },
+        main = "ibl",
         opts = {
-            char = "│",
-            filetype_exclude = { "help", "alpha", "dashboard", "neo-tree", "Trouble", "lazy", "mason" },
-            show_trailing_blankline_indent = false,
-            show_current_context = false,
+            exclude = {
+                filetypes = {
+                    "alpha",
+                    "dashboard",
+                    "help",
+                    "lazy",
+                    "mason",
+                    "neo-tree",
+                    "trouble",
+                },
+            },
+            indent = { char = "│" },
+            scope = {
+                show_end = false,
+                highlight = { "Function", "Label" },
+            },
         },
-        config = function()
-            require("indent_blankline").setup {
-                show_current_context = true,
-                show_current_context_start = true,
-                space_char_blankline = " ",
-            }
-        end,
     },
 }
