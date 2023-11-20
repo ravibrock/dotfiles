9a3ac63154d5dbbbae4b99fa488c3e01b7439794
ravibrock <66334356+ravibrock@users.noreply.github.com>
2023-11-20 16:57:43 -0500

    Refactor vimtex config + hopefully reduce edit lag

--- nvim/lua/plugins/startup/infra.lua
+++ nvim/lua/plugins/startup/infra.lua
@@ -193,32 +193,7 @@ return {
             { "<leader>lc", "<CMD>VimtexClean<CR>" },
         },
         init = function()
-            vim.g.vimtex_view_method = "sioyek"
-            vim.g.vimtex_view_sioyek_options = "--new-window"
-            -- vim.g.vimtex_compiler_latexmk = { options = { "-shell-escape" } } -- Disable by default
-            vim.keymap.set("i", "<C-x><CR>", "<plug>(vimtex-delim-close)", { silent = true })
-            vim.opt.conceallevel = 2 -- Enable VimTeX conceal
-
-            vim.api.nvim_create_autocmd("User", {
-                pattern = "VimtexEventQuit",
-                desc = "VimTeX: Clean up auxiliary files on exit",
-                command = "VimtexClean"
-            })
-            vim.api.nvim_create_autocmd("User", {
-                pattern = "VimtexEventViewReverse",
-                desc = "VimTeX: Return focus to vim after inverse search trigger in PDF",
-                command = vim.fn.system("osascript -e 'activate application \"iTerm\"'"),
-            })
-            vim.api.nvim_create_autocmd("User", {
-                pattern = "VeryLazy",
-                desc = "VimTeX: Reload to fix symlink behavior",
-                command = "silent! VimtexReload",
-            })
-            vim.api.nvim_create_autocmd("InsertLeave", {
-                pattern = "*.tex, *.bib",
-                desc = "VimTeX: Auto replace double quotes in current line with `` and ''",
-                command = 'silent! s/\\"\\([^\\"]*\\)\\"/\\`\\`\\1\'\'/'
-            })
+            require(prefix .. "vimtex")
         end,
     },
     {
