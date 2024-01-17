1446e596744a505a2676f27e25ae055cd1ca81e4
ravibrock <66334356+ravibrock@users.noreply.github.com>
2024-01-17 09:36:47 -0500

    Improve autocmds (esp. VimTeX quote replacement)

--- nvim/lua/config/autocommands.lua
+++ nvim/lua/config/autocommands.lua
@@ -8,19 +8,20 @@ vim.opt.autoread = true
 autocmd({ "FocusGained", "BufEnter" }, { command = "silent! checktime" })
 
 -- Adjust highlighting
-function ClearHL()
-    vim.cmd("highlight clear SpellBad")
-    vim.cmd("highlight clear SpellCap")
-    vim.cmd("highlight clear SpellRare")
-    vim.cmd("highlight clear SpellLocal")
-    vim.cmd("highlight clear DiagnosticUnderlineError")
-    vim.cmd("highlight clear DiagnosticUnderlineWarn")
-    vim.cmd("highlight clear DiagnosticUnderlineInfo")
-    vim.cmd("highlight clear DiagnosticUnderlineHint")
-    vim.cmd("highlight clear DiagnosticUnderlineOk")
-    vim.cmd("highlight! link FoldColumn LineNr")
-end
-autocmd("BufReadPre", { command = "lua ClearHL()" })
+autocmd("BufReadPre", {
+    callback = function()
+        vim.cmd("highlight clear SpellBad")
+        vim.cmd("highlight clear SpellCap")
+        vim.cmd("highlight clear SpellRare")
+        vim.cmd("highlight clear SpellLocal")
+        vim.cmd("highlight clear DiagnosticUnderlineError")
+        vim.cmd("highlight clear DiagnosticUnderlineWarn")
+        vim.cmd("highlight clear DiagnosticUnderlineInfo")
+        vim.cmd("highlight clear DiagnosticUnderlineHint")
+        vim.cmd("highlight clear DiagnosticUnderlineOk")
+        vim.cmd("highlight! link FoldColumn LineNr")
+    end,
+})
 
 -- Set filetype for output, error, and strace files
 autocmd("BufRead", { command = "if expand('%:e') == 'out' | set ft=log | endif" })
@@ -28,11 +29,12 @@ autocmd("BufRead", { command = "if expand('%:e') == 'err' | set ft=log | endif" 
 autocmd("BufRead", { command = "if getline(1) =~ '^execve' | set ft=strace | endif" })
 
 -- Remove trailing whitespace on save
-function CleanSpaces()
-    local save_cursor = vim.fn.getpos(".")
-    local old_query = vim.fn.getreg("/")
-    vim.cmd("silent %s/\\s\\+$//e")
-    vim.fn.setpos(".", save_cursor)
-    vim.fn.setreg("/", old_query)
-end
-autocmd("BufWritePre", { command = "lua CleanSpaces()" })
+autocmd("BufWritePre", {
+    callback = function()
+        local save_cursor = vim.fn.getpos(".")
+        local old_query = vim.fn.getreg("/")
+        vim.cmd("silent %s/\\s\\+$//e")
+        vim.fn.setpos(".", save_cursor)
+        vim.fn.setreg("/", old_query)
+    end,
+})
--- nvim/lua/plugins/opts/infra/vimtex.lua
+++ nvim/lua/plugins/opts/infra/vimtex.lua
@@ -6,17 +6,6 @@ vim.g.vimtex_fold_enabled = 2
 vim.keymap.set("i", "<C-x><CR>", "<plug>(vimtex-delim-close)", { silent = true, desc = "Close delimiter" })
 vim.opt.conceallevel = 2 -- Enable VimTeX conceal
 
--- Replace double quotes with `` and ''
-local function quotes()
-    local line = vim.fn.getline(".")
-    local position = vim.api.nvim_win_get_cursor(0)[2] + 1
-    local _, quote_count = string.sub(line, 1, position):gsub('"', '')
-    vim.cmd('silent! s/\\"\\([^\\"]*\\)\\"/\\`\\`\\1\'\'/')
-    local new_line = vim.fn.getline(".")
-    if line ~= new_line then vim.fn.cursor({ ".", position + quote_count }) end
-end
-vim.api.nvim_create_user_command("VimtexQuotes", quotes, {})
-
 vim.api.nvim_create_augroup("VimTeX", {})
 vim.api.nvim_create_autocmd("User", {
     group = "VimTeX",
@@ -34,11 +23,19 @@ vim.api.nvim_create_autocmd("User", {
     group = "VimTeX",
     pattern = "VimtexEventViewReverse",
     desc = "VimTeX: Send cursor to last column after inverse search trigger in PDF",
-    command = "lua vim.fn.cursor(vim.fn.line('.'), vim.fn.col('$'))",
+    callback = function() vim.fn.cursor(".", vim.fn.col("$")) end,
 })
 vim.api.nvim_create_autocmd("InsertLeave", {
     group = "VimTeX",
     pattern = { "*.tex", "*.bib" },
     desc = "VimTeX: Auto replace double quotes in current line with `` and ''",
-    command = "VimtexQuotes",
+    callback = function()
+        local line, linenr = vim.fn.getline("."), vim.fn.line(".")
+        local position = vim.api.nvim_win_get_cursor(0)[2] + 1
+        local _, quote_count = string.sub(line, 1, position):gsub('"', '')
+        vim.cmd('silent! %s/\\"\\([^\\"]*\\)\\"/\\`\\`\\1\'\'/')
+        ---@diagnostic disable-next-line: param-type-mismatch
+        local new_line = vim.fn.getline(linenr)
+        if line ~= new_line then vim.fn.cursor({ linenr, position + quote_count }) end
+    end,
 })
